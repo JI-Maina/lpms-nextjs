c63ef603b93c475ec50380e2cf866db715d1d941
JI-Maina <irungujmaina@gmail.com>
2023-11-30 09:54:21 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@components/ui/button";
+
+function App() {
+  return (
+    <main className="">
+      Home Page
+      <Button>Click Me</Button>
+    </main>
+  );
+}
+
+export default App;
