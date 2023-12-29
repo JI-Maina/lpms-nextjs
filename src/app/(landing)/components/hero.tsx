d0b0541388d691c18a8bcb9ffd967f1d6e2c13c6
JI-Maina <irutujmaina@gmail.com>
2023-12-29 08:01:59 +0300

    Created navbar component for the landing page

--- src/app/(landing)/components/hero.tsx
+++ src/app/(landing)/components/hero.tsx
@@ -1,14 +1,14 @@
 const Hero = () => {
   return (
     <section className="h-screen bg-hero bg-no-repeat bg-cover bg-center relative">
-      <div className=" bg-black bg-opacity-70 absolute inset-0"></div>
+      <div className="bg-black bg-opacity-70 absolute inset-0"></div>
 
       <div className="absolute bottom-8 px-2 md:px-24 max-w-[990px]">
         <h1 className="md:text-5xl text-3xl mb-8 md:mb-16">
           Take your property management to the next level
         </h1>
 
-        <button className="border p-2 border-white rounded-sm hover:bg-slate-100 hover:text-black">
+        <button className="border p-2 border-[#A020F0] rounded-sm hover:bg-[#A020F0] ">
           Learn about LPMS
         </button>
       </div>
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/header";
 import Hero from "./components/hero";
 
 export default function Home() {
   return (
     <main className="">
+      <Header />
       <Hero />
     </main>
   );
