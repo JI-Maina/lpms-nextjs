345c245939d1fa6229ccdfc8cb66dbe70ad34074
JI-Maina <irutujmaina@gmail.com>
2023-12-29 06:31:57 +0300

    created hero section for the landing page

---
+++ src/app/(landing)/components/hero.tsx
@@ -0,0 +1,19 @@
+const Hero = () => {
+  return (
+    <section className="h-screen bg-hero bg-no-repeat bg-cover bg-center relative">
+      <div className=" bg-black bg-opacity-70 absolute inset-0"></div>
+
+      <div className="absolute bottom-8 px-2 md:px-24 max-w-[990px]">
+        <h1 className="md:text-5xl text-3xl mb-8 md:mb-16">
+          Take your property management to the next level
+        </h1>
+
+        <button className="border p-2 border-white rounded-sm hover:bg-slate-100 hover:text-black">
+          Learn about LPMS
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <main>LPMS Landing Page</main>;
-}
