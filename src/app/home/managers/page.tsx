969cda4ca46b6a018b7bc4d913dc260ead81e094
JI-Maina <irutujmaina@gmail.com>
2024-01-23 09:44:11 +0300

    Created managers homepage components

--- src/app/home/managers/page.tsx
+++ src/app/home/managers/page.tsx
@@ -1,5 +1,40 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import HighlightCards from "./components/highlight-cards";
+import { Overview } from "./components/overview";
+import { RecentMaintenances } from "./components/recent-maintenances";
+
 const ManagersHomePage = () => {
-  return <div>ManagersHomePage</div>;
+  return (
+    <div className="mt-3 space-y-4">
+      <HighlightCards />
+
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle>Overview</CardTitle>
+          </CardHeader>
+          <CardContent className="pl-2">
+            <Overview />
+          </CardContent>
+        </Card>
+        <Card className="col-span-3">
+          <CardHeader>
+            <CardTitle>Recent Maintenances</CardTitle>
+            <CardDescription>You made 265 sales this month.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <RecentMaintenances />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default ManagersHomePage;
