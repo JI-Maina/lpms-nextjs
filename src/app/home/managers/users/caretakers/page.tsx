fb15b87a84e1a060cd8d01d00b338da42fd42608
McMaina <irutujmaina@gmail.com>
2024-02-27 07:43:38 +0300

    Added a banner to the caretakers page

--- src/app/home/managers/users/caretakers/page.tsx
+++ src/app/home/managers/users/caretakers/page.tsx
@@ -2,14 +2,23 @@ import { Caretaker } from "@/types/property";
 import { columns } from "../../components/caretakers/columns";
 import { getAllCaretakers } from "@/lib/data-fetching/fetch-caretakers";
 import { CaretakersTable } from "../../components/caretakers/caretakers-table";
+import CreateCaretakerDialog from "../../components/caretakers/create-caretaker-dialog";
 
 const CaretakersPage = async () => {
   const caretakersData: Promise<Caretaker[]> = getAllCaretakers();
   const caretakers = await caretakersData;
 
   return (
-    <main className="max-w-[360px] sm:max-w-full">
-      <CaretakersTable data={caretakers} columns={columns} />
+    <main className="space-y-4">
+      <div className="h-36 bg-property bg-cover bg-center bg-no-repeat bg-opacity-5 relative">
+        <div className="flex justify-end absolute right-0 bottom-0 p-1">
+          <CreateCaretakerDialog />
+        </div>
+      </div>
+
+      <div className="max-w-[360px] sm:max-w-full">
+        <CaretakersTable data={caretakers} columns={columns} />
+      </div>
     </main>
   );
 };
