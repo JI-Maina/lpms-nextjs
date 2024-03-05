5068dd369bf643023caf30e07b6e39475867ccdf
McMaina <irutujmaina@gmail.com>
2024-03-05 16:25:15 +0300

    Added error handling file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,17 @@
+// import { getServerSession } from "next-auth";
+// import { NextRequest, NextResponse } from "next/server";
+// import { authOptions } from "./app/api/auth/[...nextauth]/options";
+
 export { default } from "next-auth/middleware";
+
+// export async function middleware(request: NextRequest) {
+//     const session = await getServerSession(authOptions);
+
+//     if (session?.user) {
+//         return NextResponse.next();
+//     }
+
+//     return NextResponse.redirect(new URL('/auth/login', request.url));
+// }
 
 export const config = { matcher: ["/home/:path*"] };
